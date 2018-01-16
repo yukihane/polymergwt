53067c756fee5168e5e4b75de684db66c2e9fa1d
yukihane <yukihane.feather@gmail.com>
2018-01-17 03:15:18 +0900

    original sources bug fix: Event does not have detail element

--- src/main/java/com/google/gwt/polymerdemo/myelemental/Event.java
+++ src/main/java/com/google/gwt/polymerdemo/myelemental/Event.java
@@ -1,14 +1,7 @@
 package com.google.gwt.polymerdemo.myelemental;
 
-import jsinterop.annotations.JsProperty;
 import jsinterop.annotations.JsType;
 
 @JsType(isNative=true)
 public interface Event {
-  
-  @JsProperty Detail getDetail();
-
-  @JsType(isNative=true)
-  public interface Detail {
-  }
 }
--- src/main/java/com/google/gwt/polymerdemo/polymerstubs/CoreResponsiveChangeEvent.java
+++ src/main/java/com/google/gwt/polymerdemo/polymerstubs/CoreResponsiveChangeEvent.java
@@ -8,12 +8,11 @@ import jsinterop.annotations.JsType;
 @JsType(isNative=true)
 public interface CoreResponsiveChangeEvent extends Event {
 
-  @Override
   @JsProperty
   Detail getDetail();
 
   @JsType(isNative=true)
-  interface Detail extends Event.Detail {
+  interface Detail {
     // TODO(digi): Fix JsProperty bean parsing to allow this to be renamed isNarrow.
     @JsProperty boolean getNarrow();
   }
